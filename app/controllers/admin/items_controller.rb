fd5b7411072552edfc624fcb9883354b5079ff0c
EC2 Default User <ec2-user@ip-172-31-65-55.ec2.internal>
2020-11-23 11:48:03 +0000

    [Finish]商品編集画面完了

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -19,11 +19,18 @@ class Admin::ItemsController < ApplicationController
   end
 
   def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    redirect_to admin_item_path(@item)
   end
 
   private
   def item_params
-    params.require(:item).permit(:image_id, :name, :introduction, :price)
+    params.require(:item).permit(:image, :name, :introduction, :price,)
   end
 
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'eba5714a93fb3058f2606483831d3d3280932b873e2e1b8c8a446994c3529149a6d662235ffdf2941041bbd9a5ef9f1d737fe309cc28fefd50f3cd3e13f205ed'
