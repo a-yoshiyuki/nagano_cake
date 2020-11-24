d88428afc611226f562f894f14734bd19e4d7986
EC2 Default User <ec2-user@ip-172-31-65-55.ec2.internal>
2020-11-24 07:25:22 +0000

    [Start]注文履歴一覧　開始

--- app/controllers/admin/end_users_controller.rb
+++ app/controllers/admin/end_users_controller.rb
@@ -15,6 +15,13 @@ class Admin::EndUsersController < ApplicationController
     @customer = Customer.find(params[:id])
   end
 
+  def update
+    @customer = Customer.find(params[:id])
+    @customer.update(customer_params)
+    redirect_to admin_end_user_path(@customer)
+  end
+
+
   private
   def customer_params
     params.require(:customer).permit(:email, :last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number)
