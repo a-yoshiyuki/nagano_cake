772b75bb756078aa89c5a43d5c56834fb34f3f02
EC2 Default User <ec2-user@ip-172-31-65-55.ec2.internal>
2020-11-25 08:44:19 +0000

    [Finish]ログイン画面　完了

--- config/routes.rb
+++ config/routes.rb
@@ -29,8 +29,16 @@ Rails.application.routes.draw do
     root 'homes#top'
     get '/admins/sign_out' => 'devise/sessions#destroy'
   end
-  devise_for :admins
-  devise_for :customers
+  devise_for :admins, controllers: {
+  sessions:      'admins/sessions',
+  passwords:     'admins/passwords',
+  registrations: 'admins/registrations'
+}
+  devise_for :customers, controllers: {
+  sessions:      'customers/sessions',
+  passwords:     'customers/passwords',
+  registrations: 'customers/registrations'
+}
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
