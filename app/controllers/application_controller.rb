3a92dfb460939e36e9d134b22fbda838d32d1316
EC2 Default User <ec2-user@ip-172-31-65-55.ec2.internal>
2020-11-26 12:24:08 +0000

    [Start]ユーザー側商品一覧gamenn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,12 @@
 class ApplicationController < ActionController::Base
 
   def after_sign_in_path_for(resource)
-    admin_items_path(resource)
+    case resource
+     when Admin
+      admins_home_path
+     when Customer
+      public_end_user_path(@customer)
+    end
   end
 
   def after_sign_up_path_for(resource)
