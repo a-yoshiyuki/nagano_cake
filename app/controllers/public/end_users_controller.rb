cb3a43b92c8854276fd0ebe166e71dcf62377a0b
EC2 Default User <ec2-user@ip-172-31-65-55.ec2.internal>
2020-11-26 12:24:08 +0000

    [Start]ユーザー側商品一覧gamenn

--- app/controllers/public/end_users_controller.rb
+++ app/controllers/public/end_users_controller.rb
@@ -1,10 +1,17 @@
 class Public::EndUsersController < ApplicationController
   def show
+     @customer = Customer.find(params[:id])
+     @customer = Customer.new
   end
 
   def edit
   end
 
   def unsubscribe
+  end
+
+  private
+  def customer_params
+    params.require(:customer).permit(:email, :last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number)
   end
 end
