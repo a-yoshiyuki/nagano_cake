afb540805b02df8325ac69172e9846202cf938f6
EC2 Default User <ec2-user@ip-172-31-65-55.ec2.internal>
2020-11-29 16:42:41 +0000

    [Start]public商品一覧　作成開始

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -15,7 +15,7 @@ class Admin::ItemsController < ApplicationController
   def create
     @item = Item.new(item_params)
     @item.save
-    redirect_to admin_items_index_path
+    redirect_to admin_items_path
   end
 
   def edit
