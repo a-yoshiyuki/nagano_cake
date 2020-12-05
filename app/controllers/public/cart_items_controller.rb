5a73b4cad4cd39d0ed524cb924793797176ab012
EC2 Default User <ec2-user@ip-172-31-65-55.ec2.internal>
2020-12-05 09:51:36 +0000

    [Add]カートページ　商品追加機能　完了

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -7,8 +7,14 @@ class Public::CartItemsController < ApplicationController
 
   def create
     @cart_item = CartItem.new(cart_item_params)
-    @cart_item.customer_id = current_customer.id
-    @cart_item.save
+    if CartItem.find_by(customer_id: current_customer.id, item_id: params[:cart_item][:item_id]).present?
+      present_ci = CartItem.find_by(customer_id: current_customer.id, item_id: params[:cart_item][:item_id])
+      present_ci.amount = present_ci.amount.to_i + params[:cart_item][:amount].to_i
+      present_ci.update(amount: present_ci.amount)
+    else
+      @cart_item.customer_id = current_customer.id
+      @cart_item.save
+    end
     redirect_to public_cart_items_path
   end
 
