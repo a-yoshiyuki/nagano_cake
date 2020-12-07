e3746e6e7b68827ccd7514bf6c9fff0c902b5901
EC2 Default User <ec2-user@ip-172-31-65-55.ec2.internal>
2020-12-07 10:19:54 +0000

    [Fnish]カートページ　完了

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -3,6 +3,11 @@ class Public::CartItemsController < ApplicationController
     @costmer = current_customer
     @cart_items = @costmer.cart_items
     @cart_item = CartItem.new
+    @cart_item_price = 0
+
+    current_customer.cart_items.each do |price|
+      @cart_item_price += (price.amount * price.item.price * 1.1).round(0)
+    end
   end
 
   def create
