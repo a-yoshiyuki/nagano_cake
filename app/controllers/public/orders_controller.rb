ff148d79b1b8d2f01048275978d82c78989077b9
EC2 Default User <ec2-user@ip-172-31-65-55.ec2.internal>
2020-12-10 10:22:09 +0000

    [Finish]注文情報入力画面　完成

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,17 +1,30 @@
 class Public::OrdersController < ApplicationController
-  def new
-  end
+    def new
+      @order = Order.new
+    end
 
-  def confirmation
-    @payment = Order.new
-  end
+    def confirmation
+      @order = Order.new(order_params)
+      @order.customer_id = current_customer.id
+      @order.postal_code = current_customer.postal_code
+      @order.address = current_customer.address
+      @order.shipping = 0
+      @order.status = 0
+      
+    end
 
-  def complete
-  end
+    def complete
+    end
 
-  def index
-  end
+    def index
+    end
 
-  def show
-  end
+    def show
+    end
+
+  private
+    def order_params
+      params.require(:order).permit(:customer_id, :payment)
+    end
+
 end
