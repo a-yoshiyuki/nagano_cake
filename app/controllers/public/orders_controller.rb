a1aeaf0527b5aec9b33ccfa7cf933f2478585819
EC2 Default User <ec2-user@ip-172-31-65-55.ec2.internal>
2020-12-18 15:28:32 +0000

    [Finish]注文履歴詳細画面　完成

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -64,7 +64,15 @@ class Public::OrdersController < ApplicationController
 
     def show
       @order = Order.find(params[:id])
-      
+      @order_item_price = 0
+      @order.order_items.each do |price|
+        @order_item_price += (price.quantity.to_i * price.purchase_price * 1.1).round(0)
+      end
+
+      @total_price = 0
+      @total_price += (@order_item_price + 800 )
+
+
     end
 
 
--- app/models/order.rb
+++ app/models/order.rb
@@ -5,5 +5,7 @@ class Order < ApplicationRecord
 
   enum payment: { クレジットカード: 0, 銀行振込: 1 }
 
-
+  def syoukei
+    (self.order_items.price * 1.1 * self.quantity.to_i).round(0)
+  end
 end
