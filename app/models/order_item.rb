6e8343cbbdbcc6d8df0c94b7a34f99527df43842
EC2 Default User <ec2-user@ip-172-31-65-55.ec2.internal>
2020-12-19 01:38:05 +0000

    [Add]注文履歴詳細画面　編集

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -3,4 +3,11 @@ class OrderItem < ApplicationRecord
   belongs_to :order
   belongs_to :item
 
+  def syoukei
+    (self.item.price * 1.1 * self.quantity.to_i).round(0)
+  end
+
+  def tannka
+    (self.purchase_price * 1.1).round(0)
+  end
 end
