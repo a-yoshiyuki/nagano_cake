8c16792ff5ac14de20097e5738eb15c6ebb5af84
EC2 Default User <a.yoshiyuki3285@gmail.com>
2021-01-10 13:28:12 +0000

    [Modify]db:migrate

--- db/migrate/20201119082202_create_orders.rb
+++ db/migrate/20201119082202_create_orders.rb
@@ -1,14 +1,14 @@
 class CreateOrders < ActiveRecord::Migration[5.2]
   def change
     create_table :orders do |t|
-      t.integer :customer_id, null: false, default: ""
+      t.integer :customer_id, null: false
       t.string :postal_code, null: false, default: ""
       t.string :address, null: false, default: ""
       t.string :name, null: false, default: ""
       t.string :shipping, null: false, default: ""
       t.string :billing_amount, null: false, default: ""
-      t.integer :status, null: false, default: ""
-      t.integer :payment, null: false, default: ""
+      t.integer :status, null: false
+      t.integer :payment, null: false
 
       t.timestamps
     end
--- db/migrate/20201119082621_create_order_items.rb
+++ db/migrate/20201119082621_create_order_items.rb
@@ -1,11 +1,11 @@
 class CreateOrderItems < ActiveRecord::Migration[5.2]
   def change
     create_table :order_items do |t|
-      t.integer :item_id, null: false, default: ""
-      t.integer :order_id, null: false, default: ""
-      t.integer :purchase_price, null: false, default: ""
-      t.string :quantity, null: false, default: ""
-      t.integer :status, null: false, default: ""
+      t.integer :item_id, null: false
+      t.integer :order_id, null: false
+      t.integer :purchase_price, null: false
+      t.string :quantity, null: false
+      t.integer :status, null: false
 
       t.timestamps
     end
